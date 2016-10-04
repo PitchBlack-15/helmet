ee6e5dac56e8bfb147ebec072945621ea78d9680
PitchBlack-15 <PitchBlack-15@users.noreply.github.com>
2016-10-04 12:40:43 +0800

    Add files via upload

--- Helmet.ino
+++ Helmet.ino
@@ -1,34 +1,70 @@
 #include <VirtualWire.h>
 #include <IRremote.h>
-#define PIN_IR 3
+
 char *controller;
-IRsend irsend;
+
+
+
+int PIN_IR = 3;
 int LowBatOut = 5;
 int BtnCancel = 7;
 int AlcoholSensor = 15;
+int vibrationSensor = 9;
+int BtnCancelState = 0; 
 
 void setup() {
-  irsend.enableIROut(38);
-  irsend.mark(0);
+  pinMode(PIN_IR, OUTPUT);
+  pinMode(BtnCancel, INPUT);
+  pinMode(LowBatOut, INPUT);
+  pinMode(AlcoholSensor, INPUT);
+  pinMode(vibrationSensor, INPUT);
+  Serial.begin(9600); //Set serial baud rate to 9600 bps
   vw_set_ptt_inverted(true); //
   vw_set_tx_pin(12);
   vw_setup(4000);// speed of data transfer Kbps
 }
 
 void loop() {
-  controller = "L"  ;
-  vw_send((uint8_t *)controller, strlen(controller));
-  vw_wait_tx(); // Wait until the whole message is gone
-  delay(2000);
+  long measurement = TP_init();
+  BtnCancelState = digitalRead(BtnCancel);
+  int val;
+  val=analogRead(0);//Read Gas value from analog 0
+  Serial.println(val,DEC);//Print the value to serial port
   
-  controller = "C"  ;
-  vw_send((uint8_t *)controller, strlen(controller));
-  vw_wait_tx(); // Wait until the whole message is gone
-  delay(2000);
   
-  controller = "B"  ;
+  delay(50);
+  if (measurement > 1000) {
+    controller = "C"  ; //helmet Crash/bump
+    vw_send((uint8_t *)controller, strlen(controller));
+    vw_wait_tx(); // Wait until the whole message is gone
+    delay(1000);
+  }
+
+  if (BtnCancelState == HIGH) {
+      controller = "B"  ; // Cancel button pressed
+      vw_send((uint8_t *)controller, strlen(controller));
+      vw_wait_tx(); // Wait until the whole message is gone
+      delay(1000);
+  }
+
+
+  controller = "L"  ; //Low Battery
   vw_send((uint8_t *)controller, strlen(controller));
   vw_wait_tx(); // Wait until the whole message is gone
   delay(2000);
 
+
+
+  controller = "A"  ; // Drunk Driver
+  vw_send((uint8_t *)controller, strlen(controller));
+  vw_wait_tx(); // Wait until the whole message is gone
+  delay(2000);
+
+}
+
+
+long TP_init() {
+  delay(10);
+  long measurement = pulseIn (vibrationSensor, HIGH); //wait for the pin to get HIGH and returns measurement
+  return measurement;
 }
